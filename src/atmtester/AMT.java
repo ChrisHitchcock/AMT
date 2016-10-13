aecf3d49d76587a70a2c5b7b22a8911c685b3750
chhit5249 <chhit5249@7JGN6Y1.Student.UGDSB.ED>
2016-10-13 13:48:43 -0400

    Final Version 1.0 Functional: Withdraw/Deposit/viewBal/addInterest methods working, fully documented/commented

--- src/atmtester/AMT.java
+++ src/atmtester/AMT.java
@@ -1,54 +1,90 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package atmtester;
 
 /**
- *
+ * This class is an object that represents a bank; it allows the user to deposit,
+ * withdraw, add interest, and view the balance of their bank account.
  * @author Chris
  */
 public class AMT {
-
+    //Create the details of the AMT
     private String bank;
-    private double initBal, newBal, currentBal;
+    private double currentBal;
 
+    /**
+     * This constructor takes the inputted name of the bank and the initial balance,
+     * and sets those details as the details of the AMT.
+     * @param bk Name of the bank
+     * @param ib Initial balance
+     */
     public AMT(String bk, double ib) {
         bank = bk;
         currentBal = ib;
     }
 
+    /**
+     * This method allows the user to withdraw (remove) money from their account.
+     * @param wd Amount being withdraw.
+     * @return currentBal
+     */
     public double withdraw(double wd) {
         if (wd >= 0) {
             currentBal = currentBal - wd;
             return currentBal;
         } else {
             System.out.println("Please enter a valid (positive) number.");
-            return wd;
+            return currentBal;
         }
     }
 
+    /**
+     * This method allows the user to deposit (add) money to their account.
+     * @param dp Amount being deposited
+     * @return currentBal
+     */
     public double deposit(double dp) {
         if (dp >= 0) {
             currentBal = currentBal + dp;
             return currentBal;
         } else {
             System.out.println("Please enter a valid (positive) number.");
-            return dp;
+            return currentBal;
         }
     }
-    
-    public double addInterest(double P, double i, int n)
-    {
-        double dailyInt;
-        dailyInt = P*Math.pow((1+i),n);
-        System.out.println("Your daily interest is: "+dailyInt);
-        return currentBal;
+
+    /**
+     * This method allows the user to add interest to their balance.
+     * @param P Principle amount
+     * @param i Interest rate
+     * @param n Number of periods
+     * @return currentBal
+     */
+    public double addInterest(double P, double i, int n) {
+        if (P > 0 && i > 0 && n > 0) {
+            double dailyInt;
+            dailyInt = P * Math.pow((1 + i), n);
+            currentBal += dailyInt;
+            return currentBal;
+        } else {
+            System.out.println("Please enter valid (positive) numbers.");
+            return currentBal;
+        }
     }
 
-    public String viewBal() {
-        String output = "" + currentBal;
-        return output;
+    /**
+     * This method allows the user to view the balance of their bank account
+     * @return output (balance)
+     */
+    public String viewBal(boolean fin) {
+        currentBal = currentBal * 100;
+        currentBal = Math.round(currentBal);
+        currentBal = currentBal / 100;
+        if (fin == false)
+        {
+            String output = "Your current balance is: $" + currentBal;
+            return output;
+        } else {
+            String output = "Your final balance is: $" + currentBal;
+            return output;
+        }
     }
 }
--- src/atmtester/ATMTester.java
+++ src/atmtester/ATMTester.java
@@ -5,24 +5,88 @@
  */
 package atmtester;
 
+import java.util.*;
+
 /**
+ * This program tests the AMT class.
  *
  * @author Chris
  */
 public class ATMTester {
 
     /**
+     * Everything happens here
+     *
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        AMT test = new AMT("CIBC", 1000);
-        
-        test.withdraw(-100);
-        test.deposit(200);
-        test.addInterest(1000,50,5);
-        
-        System.out.println(test.viewBal());
+        //Variables and such.
+        Scanner kb = new Scanner(System.in);
+        boolean menu;
+        String end;
+        int choice;
+
+        //Obtain the name of the bank and init. balance, and set those as the parameters of the AMT.
+        System.out.println("Please enter the name of your bank and your intial balance (individually).");
+        String bankName = kb.next();
+        AMT bank = new AMT(bankName, kb.nextDouble());
+
+        //While loop; keep popping up the menu until told not to.
+        menu = true;
+        while (menu == true) {
+            //Menu and choice selection
+            System.out.println(bankName + "\n1.\tDeposit Money\n2.\tWithdraw Money\n3.\tAdd Interest\n4.\tView Balance\n5.\tExit");
+            choice = kb.nextInt();
+
+            //If statements regarding the selected choice.
+            //Deposit money
+            if (choice == 1) {
+                int amount;
+                System.out.println("How much are you depositing?");
+                amount = kb.nextInt();
+                bank.deposit(amount);
+            } else 
+            //Withdraw money
+            if (choice == 2) {
+                int amount;
+                System.out.println("How much are you withdrawing?");
+                amount = kb.nextInt();
+                bank.withdraw(amount);
+            } else 
+            //Add interest to balance
+            if (choice == 3) {
+                double P, i;
+                int n;
+                System.out.println("Please enter the prinicpal amount, interest rate (decimal) and amount of periods individually.");
+                P = kb.nextDouble();
+                i = kb.nextDouble();
+                n = kb.nextInt();
+                bank.addInterest(P, i, n);
+            } else 
+            //View Balance
+            if (choice == 4) {
+                System.out.println(bank.viewBal(false));
+            } else
+            //Exit
+            if (choice == 5)
+            {
+                menu=false;
+            }
+            
+            //Prompt regarding the while loop. If yes, while loop ends, if no, while loop continues.
+            if (menu==true)
+            {
+                System.out.println("Are you done?");
+                end = kb.next();
+                if (end.equalsIgnoreCase("yes")) {
+                    menu = false;
+                } else {
+                    menu = true;
+                }
+            }
+        }
+        //Show the final balance
+        System.out.println("Final Balance: "+bank.viewBal(true));
     }
-    
+
 }
